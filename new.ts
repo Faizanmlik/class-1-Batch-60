3dd4c16acb2f51cddf58ed060b0469c14e6b4668
Cloud Faizan <muhammadfaizanjee1@gmail.com>
2024-07-02 01:09:48 -0700

    new ts file

---
+++ new.ts
@@ -0,0 +1,41 @@
+
+// var i :number=1
+// while(i <=10){
+// //let i:number= 1;
+// let table:number= 10*i
+
+
+// console.log("10 *", i ,"=", table)
+// i++
+
+// }
+// console.log("Breack")
+
+//                          DEFINE AGE IN DAYS ;;;;;;;;;
+
+// function age(age:number){
+// //let num:number=age *365
+// if(age %4==0){
+//     age
+//    console.log( age*366)
+// }
+// else{
+//     console.log(age*365)
+// }
+
+
+// //console.log("Your age is =",num,"days")
+// }
+
+// age(3)
+
+//                        DEFINE AGE IN HOURS ;;;;;;;;;
+
+
+
+var age:number= 3;
+const days:number= age * 365;
+let hour:number= days *24;
+console.log("your age is ",hour,"Hours")
+
+
